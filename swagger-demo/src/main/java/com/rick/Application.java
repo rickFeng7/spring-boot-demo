d2eda804a391549990171b6514fb9d23ef03c4b5
fengrui03 <45378344+rickFeng7@users.noreply.github.com>
2022-11-09 16:49:51 +0800

    Initial commit

---
+++ swagger-demo/src/main/java/com/rick/Application.java
@@ -0,0 +1,15 @@
+package com.rick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fengrui03
+ * @date 2022/11/9
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
